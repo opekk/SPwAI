3ce8bdf2381dbde70b49867425095b7fdb6c7498
opekk <maciek09874@gmail.com>
2025-03-22 20:28:08 +0100

    commir

--- lab3/zadania/zadanie1.js
+++ lab3/zadania/zadanie1.js
@@ -1,15 +1,17 @@
 const http = require('http')
 const port = 3000
 
-    function process_request(req, res){
+function process_request(req, res){
     const body = 'Witaj na platformie Node!\n'
     const content_length = body.length
+   
     res.writeHead(200, {
-    'Content-Length': content_length,
-    'Content-Type': 'text/plain'
+        'Content-Length': content_length,
+        'Content-Type': 'text/plain'
     });
+    
     res.end(body)
-    }
+ }
     const server = http.createServer(process_request)
     server.listen(port, () => console.log('Serwer działa'))
 
